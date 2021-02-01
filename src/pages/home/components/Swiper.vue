771b2ff3c78771ab18b0f5f40868d654d7f3e387
PioneerHunter <63446400+PioneerHunter@users.noreply.github.com>
2021-02-01 22:38:08 +0800

    Add files via upload

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="wrapper">
+      <swiper :option="swiperOption" v-if="showSwiper">
+          <swiper-slide v-for="item of list" :key="item.id">
+              <img class="swiper-img" :src="item.imgUrl" alt="">
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <!-- swiper-pagination-bullet -->
+      </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        loop: true
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background red !important
+    height 10px
+  .wrapper
+    overflow hidden
+    width 100%
+    height 0
+    padding-bottom 31.25%
+    .swiper-img
+        width 100%
+</style>
